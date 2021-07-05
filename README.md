fa63a289d2f7e04c1cc67403705301d0d3b3ce83
Vtres <viniciust98@outlook.com>
2021-07-05 14:02:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Front-end com ReactJS
+Este projeto utiliza como base a criação [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home(){
+    return(
+        <div className="">
+            <h1>Ola pessoas</h1>
+            <p>ola mundo</p>
+        </div>
+    )
+}
